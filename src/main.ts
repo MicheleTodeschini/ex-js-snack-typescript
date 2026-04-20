02d6e2c6a4563ad0e9b011d6066cc76969ae0a4d
MicheleTodeschini <micheletodeschini01@gmail.com>
2026-04-20 15:32:02 +0200

    removed tipo for dato and add unknown

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+ // =========================
+//        SNACK 1
+// =========================
+
 /* Hai ricevuto un dato generico da un'API, ma non sai di che tipo sia… Il tuo compito è controllare il tipo del dato e stampare il valore in modo corretto.
 
 Se è una stringa: stampala in maiuscolo
@@ -8,7 +12,7 @@ Se è un booleano: stampa “Sì” o “No” in base al suo valore
 
 In tutti gli altri casi: stampa “Tipo non supportato” */
 
-let dato: string | number | boolean = null //testato facendo ciao, 23, true e false
+let dato: unknown = null //testato facendo ciao, 23, true e false
 
 if (typeof dato === 'string') {
   console.log(dato.toUpperCase());
@@ -28,3 +32,16 @@ if (typeof dato === 'string') {
   console.log('Tipo non supportato');
   
 }
+
+
+ // =========================
+//        SNACK 2
+// =========================
+
+/* Crea un type alias Dipendente che rappresenta un lavoratore con i seguenti dati:
+
+nome → stringa
+cognome → stringa
+annoNascita → numero
+sesso → Può essere solo "m" o "f".
+anniDiServizio (array di numeri, es. [2014, 2015, 2017, 2018]) */
